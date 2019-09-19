b07a5caeab6ffb0363569465c6e20362e6c73afd
unknown <8792613>
2019-09-18 19:17:51 -0500

    Updating Repo from Batch

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-xcopy I:\fkumi H:\DB_ClassRepo /S /E /Y
-xcopy I:\ITSE2309  H:\DB_ClassRepo /S /E /Y
+xcopy I:\ H:\DB_ClassRepo /S /E /Y
+
 git add -A
 git commit -m "Updating Repo from Batch"
 git push origin master
