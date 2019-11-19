e6447b2f5cbc43b31938a91319665df35a18ec79
unknown <8792613>
2019-11-18 18:30:34 -0600

    fixed question 8 on lab 9

--- Labs/LuisFernandoSuarezHarasic_Lab9/LuisFernandoSuarezHarasicLab09.sql
+++ Labs/LuisFernandoSuarezHarasic_Lab9/LuisFernandoSuarezHarasicLab09.sql
@@ -45,7 +45,8 @@ FROM   books
        JOIN publisher using(pubid) 
 WHERE  category IN( 'COMPUTER', 'CHILDREN' ) 
 GROUP  BY NAME, 
-          category; 
+          category 
+HAVING Avg(retail) > 50; 
 
 -- 9. 
 SELECT DISTINCT firstname, 
@@ -68,67 +69,3 @@ FROM   books
        JOIN author using(authorid) 
 WHERE  lname = 'WHITE' 
        AND fname = 'LISA'; 
-
-
--- -- 1.
-
--- SELECT COUNT(*)
--- FROM books
--- WHERE category = 'COOKING';
-
--- -- 2.
-
--- SELECT COUNT(*)
--- FROM books
--- WHERE retail > 30;
-
--- -- 3.
-
--- SELECT MAX(pubdate)
--- FROM books;
-
--- -- 4.
-
--- SELECT SUM((retail-cost)*quantity)
--- FROM orders JOIN orderitems USING(order#) JOIN books USING(isbn)
--- WHERE customer# = 1017;
-
--- -- 5.
-
--- SELECT MIN(retail)
--- FROM books
--- WHERE category = 'COMPUTER';
-
--- -- 6.
-
--- SELECT AVG(SUM((retail-cost)*quantity))
--- FROM orders JOIN orderitems USING(order#) JOIN books USING(isbn)
--- GROUP BY order#;
-
--- -- 7.
-
--- SELECT customer#, COUNT(*)
--- FROM orders
--- GROUP BY customer#;
-
--- -- 8.
-
--- SELECT name, category, AVG(retail)
--- FROM books JOIN publisher USING(pubid)
--- WHERE category IN('COMPUTER', 'CHILDREN') 
--- GROUP BY name, category;
-
--- -- 9.
-
--- SELECT DISTINCT firstname, lastname
--- FROM customers JOIN orders USING(customer#) JOIN orderitems USING(order#) JOIN books USING(isbn)
--- WHERE (state = 'FL' or state = 'GA')
--- GROUP BY order#, firstname, lastname
--- HAVING SUM(retail*quantity)>80;
-
--- -- 10.
-
--- SELECT MAX(retail)
--- FROM books JOIN bookauthor USING(isbn) JOIN author USING(authorid)
--- WHERE lname = 'WHITE'
--- AND fname = 'LISA';
